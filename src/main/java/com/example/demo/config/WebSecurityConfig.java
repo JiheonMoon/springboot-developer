a2c5f817fe95e7e0cd66be02971aca27d306a7f4
JiheonMoon <wlgjs566@gmail.com>
2024-10-21 11:17:50 +0900

    cors-2

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -52,8 +52,8 @@ public class WebSecurityConfig {
       //React 애플리케이션이 실행되는 출처에서 오는 요청을 허용
       configuration.setAllowedOrigins(Arrays.asList(
               "http://localhost:3000",
-        		  "http://todo-moondev-test.shop",
-        		  "https://todo-moondev-test.shop"));
+        		  "http://app.todo-moondev-test.shop",
+        		  "https://app.todo-moondev-test.shop"));
       //HTTP메서드 허용
       configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
       //모든 헤더를 허용
